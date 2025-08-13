8adac39ee66799f837c25270601c558ce1a8ffdc
creation <creation2224@gmail.com>
2025-08-13 23:28:25 +0530

    remaining updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,14 +6,20 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           
-          {/* Logo / Title */}
-          <div className="flex-shrink-0">
-            <h1 className="text-xl font-bold text-gray-800">PromptHelp</h1>
+          {/* Logo */}
+          <div className="flex-shrink-0 flex items-center space-x-3">
+            <img
+              src="/complex-engineering-global-svgrepo-com.svg" // Path from public folder
+              alt="PromptHelp Logo"
+              className="h-10 w-auto" // bigger logo
+            />
+            <span className="text-2xl font-extrabold text-gray-900">
+              PromptHelp
+            </span>
           </div>
 
           {/* Social Icons */}
           <div className="flex items-center space-x-4">
-            
             {/* Twitter Icon */}
             <a
               href="https://x.com/_Creation22"
